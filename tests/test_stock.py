d62ed6f512affa00f56b5b526d5992c7917eba6b
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2022-11-04 00:08:54 -0300

    Add test_stock

---
+++ tests/test_stock.py
@@ -0,0 +1,22 @@
+from app import stock
+
+
+# {
+#     "product_name": ["Chocolate", "Granizado", "Limon", "Dulce de Leche"],
+#     "quantity": [3, 10, 0, 5],
+# }
+
+
+def test_stock():
+    assert stock.is_product_available(12, 1) == False
+    assert stock.is_product_available("Chocolate", "12") == False
+    assert stock.is_product_available("Chocolate", 1.1) == False
+    assert stock.is_product_available("Chocolate", -1) == False
+    assert stock.is_product_available("Chocolate", 0) == False
+    assert stock.is_product_available("Chocolate", 1) == True
+    assert stock.is_product_available("Chocolate", 2) == True
+    assert stock.is_product_available("Chocolate", 3) == True
+    assert stock.is_product_available("Chocolate", 4) == False
+    assert stock.is_product_available("*", 1) == False
+    assert stock.is_product_available("Limon", 0) == False
+    assert stock.is_product_available("Dulce de Leche", 5) == True
